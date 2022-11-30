26d8496d600bec49f9d3a90efd2341b566d3eaff
ahmedfaresabdelghani <56740338+ahmedfaresabdelghani@users.noreply.github.com>
2022-11-30 21:09:31 +0200

    Delete BIT_MATH.h

--- BIT_MATH.h
+++
@@ -1,4 +0,0 @@
-#define SET_BIT(VAR,BIT)     (VAR |=(1<<BIT))
-#define CLR_BIT(VAR,BIT)     (VAR &=~(1<<BIT))
-#define TOGGLE_BIT(VAR,BIT)  (VAR ^=(1<<BIT))
-#define GET_BIT(VAR,BIT)     ((VAR>>BIT)&1)
