78c2e8819bbac04bde90a378ba2b06501e761076
ahmedfaresabdelghani <56740338+ahmedfaresabdelghani@users.noreply.github.com>
2022-11-30 21:11:23 +0200

    Delete p6.c

--- p6.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-    int num1,num2,num3;
-    printf("please enter num1 & num2 respectively ");
-	scanf("%d \n %d",&num1,&num2);
-	num3=num1==num2;
-    printf("%d",num3);
-}
